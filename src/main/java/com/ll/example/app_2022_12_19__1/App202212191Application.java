510414bc21bbc1080f133fa708b6485884faa6d6
DESKTOP-1BE0UPE\iwmis <iwmism1938@naver.com>
2022-12-19 10:13:18 +0900

    first commit

---
+++ src/main/java/com/ll/example/app_2022_12_19__1/App202212191Application.java
@@ -0,0 +1,13 @@
+package com.ll.example.app_2022_12_19__1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App202212191Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App202212191Application.class, args);
+    }
+
+}
